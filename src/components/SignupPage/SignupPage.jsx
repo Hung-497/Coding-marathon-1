10c5a860d46cd869b4e9c5549d5ab0dbac4e5e88
amaroqq <119763798+amaroqq@users.noreply.github.com>
2026-09-10 12:20:27 +0300

    feat: validation and self-assesment

--- src/components/SignupPage/SignupPage.jsx
+++ src/components/SignupPage/SignupPage.jsx
@@ -6,6 +6,55 @@ function SignupPage() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [nationality, setNationality] = useState('');
+  const [emailok, setEmailok] = useState(Boolean);
+  const [passok, setPassok] = useState(Boolean);
+  const [passcok, setPasscok] = useState(Boolean);
+  const [nationalityok, setNationalityok] = useState(Boolean);
+  const validateEmail = value => {
+    setEmail(value);
+    if (value.match(/^\S+@\S+\.\S+$/) != null) {
+      document.getElementById('email').style.color = 'green';
+      setEmailok(true);
+    } else {
+      setEmailok(false);
+      document.getElementById('email').style.color = 'red';
+    }
+    validateSubmit(emailok,passok,passcok,nationalityok);
+  }
+  const validatePassword = value => {
+    setPassword(value);
+    if (value.match(/[A-Z]/) && value.match(/[a-z]/) && value.match(/[0-9]/) != null) {
+      document.getElementById('password').style.color = 'green';
+      setPassok(true);
+    } else {
+      setPassok(false);
+      document.getElementById('password').style.color = 'red';
+    }
+    validateConfirm(confirmPassword);
+  }
+  const validateConfirm = value => {
+    setConfirmPassword(value);
+    if (value == password) {
+      setPasscok(true);
+      document.getElementById('confirmPassword').style.color = 'green';
+    } else {
+      setPasscok(false);
+      document.getElementById('confirmPassword').style.color = 'red';
+    }
+    validateSubmit(emailok,passok,passcok,nationalityok);
+  }
+  const validateNationality = value => {
+    setNationality(value);
+    setNationalityok(true);
+    validateSubmit(emailok,passok,passcok,true);
+  }
+  const validateSubmit = (em, p, c, n) => {
+    if (em == true && p == true && c == true && n == true) {
+      document.getElementById('submit').style.display = 'inline-block';
+    } else {
+      document.getElementById('submit').style.display = 'none';
+    }
+  }
   const onSubmit = e => {
     e.preventDefault();
     const contactUsInformation = {
@@ -16,17 +65,21 @@ function SignupPage() {
       submittedOn: new Date()
     };
     switch (contactUsInformation.nationality) {
-      case 'fi': document.getElementById('greeting').innerHTML = 'Moi!'; break;
-      case 'en': document.getElementById('greeting').innerHTML = 'Hello!'; break;
-      case 'de': document.getElementById('greeting').innerHTML = 'Hallo!'; break;
-      case 'fr': document.getElementById('greeting').innerHTML = 'Salut!'; break;
+      case 'fi': document.getElementById('greeting').innerHTML = 'Moi!'; document.getElementById('email-adress').innerHTML = `Sähköpostiosoitteesi on ${contactUsInformation.email}`; break;
+      case 'en': document.getElementById('greeting').innerHTML = 'Hello!'; document.getElementById('email-adress').innerHTML = `Your email is ${contactUsInformation.email}`; break;
+      case 'de': document.getElementById('greeting').innerHTML = 'Hallo!'; document.getElementById('email-adress').innerHTML = `Deine E-Mail-Adresse lautet ${contactUsInformation.email}`; break;
+      case 'fr': document.getElementById('greeting').innerHTML = 'Salut!'; document.getElementById('email-adress').innerHTML = `Votre adresse e-mail est ${contactUsInformation.email}`; break;
     }
-    document.getElementById('email-adress').innerHTML = `Your email is ${contactUsInformation.email}`
+
 
     setEmail('');
     setPassword('');
     setConfirmPassword('');
     setNationality('');
+    setEmailok(false);
+    setPassok(false);
+    setPasscok(false);
+    setNationalityok(false);
   };
   return (
     <div>
@@ -34,22 +87,23 @@ function SignupPage() {
       <form onSubmit={onSubmit}>
         <div>
           <label htmlFor='email'>Email:</label>
-          <input id='email' type='text' onChange={e => setEmail(e.target.value)} value={email} />
+          <input id='email' type='text' onChange={e => validateEmail(e.target.value)} value={email} required/>
         </div>
         <div>
-          <label htmlFor='password'>Password:</label>
-          <input id='password' type='password' onChange={e => setPassword(e.target.value)} value={password} />
+          <label htmlFor='password'>Password: (must contain a lowercase letter, uppercase letter and a number)</label>
+          <input id='password' type='password' onChange={e => validatePassword(e.target.value)} value={password} required/>
         </div>
         <div>
-          <label htmlFor='confirmPassword'>Confirm Password:</label>
-          <input id='confirmPassword' type='password' onChange={e => setConfirmPassword(e.target.value)} value={confirmPassword} />
+          <label htmlFor='confirmPassword'>Confirm Password: (must match password)</label>
+          <input id='confirmPassword' type='password' onChange={e => validateConfirm(e.target.value)} value={confirmPassword} required/>
         </div>
         <div>
           <label htmlFor='nationality'>Phone:</label>
           <select
             name='nationality'
-            onChange={e => setNationality(e.target.value)}
+            onChange={e => validateNationality(e.target.value)}
             value={nationality}
+            required
           >
             <option value='' disabled>
               Select a nationality...
@@ -60,7 +114,7 @@ function SignupPage() {
             <option>fr</option>
           </select>
         </div>
-        <button>Submit</button>
+        <button id='submit'>Submit</button>
       </form>
       <div id='userText'>
         <p id='greeting'></p>
